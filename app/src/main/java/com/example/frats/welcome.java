cd4dea134ed8e1e6281096ecf05b3300ffe88ccb
Daniel-Macharia <ndungudanny444@gmail.com>
2023-08-16 01:45:35 +0300

    second commit

--- app/src/main/java/com/example/frats/welcome.java
+++ app/src/main/java/com/example/frats/welcome.java
@@ -1,6 +1,5 @@
 package com.example.frats;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,34 +11,7 @@ public class welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_view);
 
-        //android.os.SystemClock.sleep(3000);
-        //sleep();
-        /*try {
-            this.wait(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }*/
-
-       // loadCreateUserView();
-
     }
 
 
-    private void sleep()
-    {
-        try {
-
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    private void loadCreateUserView()
-    {
-        Intent intent = new Intent(this, createUser.class);
-        startActivity(intent);
-        //setContentView(R.layout.home_view);
-    }
 }
